bf218d52211acd94d06c0397da451bcec448bb8f
Luciano Graziani <lgraziani2712@gmail.com>
2018-11-20 12:01:04 -0300

    Complete video#6

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,12 +1,38 @@
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
+
 import Header from './Header';
 import Meta from './Meta';
 
+const theme = {
+  red: '#f00',
+  black: '#393939',
+  grey: '#3a3a3a',
+  lightGrey: '#e1e1e1',
+  offWhite: '#ededed',
+  maxWidth: '100px',
+  // box-shadow
+  bs: '0 12px 24px 0 #0001',
+};
+
+const StyledPage = styled.div`
+  background: #fff;
+  color: ${props => props.theme.black};
+`;
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidths};
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
 export default function Page(props) {
   return (
-    <div>
-      <Meta />
-      <Header />
-      {props.children}
-    </div>
+    <ThemeProvider theme={theme}>
+      <StyledPage>
+        <Meta />
+        <Header />
+        <Inner>{props.children}</Inner>
+      </StyledPage>
+    </ThemeProvider>
   );
 }
