0ec3c434f77f34b265d9964d52fbf95a9edf5d9c
Luciano Graziani <lgraziani2712@gmail.com>
2018-11-21 10:36:32 -0300

    Complete video#8

--- sick-fits/frontend/components/Meta.js
+++ sick-fits/frontend/components/Meta.js
@@ -6,7 +6,7 @@ export default function Meta() {
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="shortcut icon" href="/static/favicon.png" />
-      <link ref="stylesheet" type="text/css" href="/static/nprogress.css" />
+      <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
       <title>Sick Fits!</title>
     </Head>
   );
